0bd03860430c837ed96d3e0fef5eda094cb76653
ZSchoen <dev@zschoen.dev>
2022-01-01 21:57:27 +0100

    improved panic & exit handling

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,39 @@
 #![warn(clippy::cognitive_complexity)]
 
 use clap::Parser;
+use human_panic::setup_panic;
 use zsplit::{split, Cli};
 
 fn main() {
+    setup_panic!();
+
     let cli = Cli::parse();
 
     if let Err(error) = cli.validate() {
         eprintln!("Error: {}", error);
-        std::process::exit(-1);
+        std::process::exit(exitcode::USAGE);
     }
 
     let mut source = match cli.source.reading_buffer() {
         Ok(source) => source,
         Err(error) => {
             eprintln!("Error: {}", error);
-            std::process::exit(-1);
+            std::process::exit(exitcode::NOINPUT);
         }
     };
 
     if let Err(error) = split(&mut source, &cli.destinations()) {
         eprintln!("Error: {}", error);
-        std::process::exit(-1);
+        use std::io::ErrorKind;
+        let code = match error.kind() {
+            ErrorKind::PermissionDenied => exitcode::NOPERM,
+            ErrorKind::AlreadyExists => exitcode::CANTCREAT,
+            // NOTE Waits for stabilization of rust-lang/rust#86442
+            // | ErrorKind::IsADirectory
+            // | ErrorKind::ReadOnlyFilesystem
+            // | ErrorKind::FilenameTooLong => exitcode::CANTCREAT,
+            _ => exitcode::IOERR,
+        };
+        std::process::exit(code);
     }
 }
